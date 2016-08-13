01333cec188391e9f6d962d5865c3ef7b4115db4
davidlazarus <davidlazarus89@gmail.com>
2016-08-13 13:22:21 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var count = 0;
+var countArray = [];
+var countArrayString =[];
+
+for (var i=1; i<=100; i++)
+  {
+    if (i%15 == 0)
+        countArray.push("fizzbuzz" + "<br/>");
+    
+    else if (i%3 == 0)
+        countArray.push("fizz" + "<br/>");
+    
+    else if (i%5 == 0)
+        countArray.push("buzz" + "<br/>");
+    
+    else
+        countArray.push(i + "<br/>");
+    
+    };
+
+console.log(countArray);
+$('.button').on('click', function(){
+  $('.main ul').append(countArray);
+});
