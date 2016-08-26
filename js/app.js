e27173377b593cbea37e6b921e8f3843b6bbb31b
davidlazarus <davidlazarus89@gmail.com>
2016-08-26 11:22:35 +0200

    final

--- js/app.js
+++ js/app.js
@@ -1,24 +1,42 @@
-var countArray = [];
-
-
-for (var i=1; i<=100; i++)
-  {
-    if (i%15 == 0)
-        countArray.push("fizzbuzz" + "<br/>");
-    
-    else if (i%3 == 0)
-        countArray.push("fizz" + "<br/>");
-    
-    else if (i%5 == 0)
-        countArray.push("buzz" + "<br/>");
-    
-    else
-        countArray.push(i + "<br/>");
-    
-    };
-
-console.log(countArray);
 $('.button').on('click', function(){
-  $('.main ul').append(countArray);
+  var userInput = parseInt($('.input').val());
+   
+  var someShit = fizzbuzz(userInput);  
+  
+  $('.main ul').append(someShit);  
 });
 
+
+function fizzbuzz(num){
+   var countArray = [];
+  
+     for (var i=1; i<=num; i++)
+  {
+    if (i%15 == 0)
+     countArray =  myPush(countArray, "fizzbuzz");
+    
+    else if (i%3 == 0)
+     countArray =  myPush(countArray, "fizz");
+    
+    else if (i%5 == 0)
+     countArray =  myPush(countArray, "buzz");
+    
+    else
+     countArray =  myPush(countArray, i);    
+    };
+   
+  return countArray;
+};
+
+
+function myPush(array, string) {
+  array.push(string + "<br/>");
+  return array;
+}
+
+
+$('.input').on('click', function(){
+ 
+  $('.main ul').html('');
+  counterArray.length = 0;   
+});
